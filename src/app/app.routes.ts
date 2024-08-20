b9b64f24854688b442168e4c7a3ef1eaa03bf982
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-08-20 12:50:27 +0200

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'homepage' },
+  { path: 'homepage', component: HomepageComponent },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { TranslateService, TranslateStore } from '@ngx-translate/core';
+import { provideRouter } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig).then((appRef) => {
+bootstrapApplication(AppComponent, {
+  providers: [...appConfig.providers, provideRouter(routes)],
+}).then((appRef) => {
   const translateService = appRef.injector.get(TranslateService);
-  const translateStore = appRef.injector.get(TranslateStore); 
+  const translateStore = appRef.injector.get(TranslateStore);
   translateService.setDefaultLang('it');
   translateService.use('it');
 });
