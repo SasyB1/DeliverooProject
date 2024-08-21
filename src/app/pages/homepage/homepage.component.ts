1024695eb5be33cbb599536890a327b4fb9cd81d
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-08-21 18:19:24 +0200

    carousel homepage start

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './homepageComponent/hero/hero.component';
+import { CarouselComponent } from './homepageComponent/carousel/carousel.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, CarouselComponent],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
