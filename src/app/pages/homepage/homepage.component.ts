34c94adafde6ff26698ce80979d3a15bc1acde45
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-08-23 09:43:05 +0200

    locate component sull homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './homepageComponent/hero/hero.component';
 import { CarouselComponent } from './homepageComponent/carousel/carousel.component';
+import { LocateComponent } from './homepageComponent/locate/locate.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeroComponent, CarouselComponent],
+  imports: [HeroComponent, CarouselComponent, LocateComponent],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
---
+++ src/app/pages/homepage/homepageComponent/locate/locate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-locate',
+  standalone: true,
+  imports: [],
+  templateUrl: './locate.component.html',
+  styleUrl: './locate.component.scss'
+})
+export class LocateComponent {
+
+}
