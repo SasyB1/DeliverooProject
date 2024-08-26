7eeddc9ba3348d0f2f7ee329ce5f48c52779e943
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-08-26 08:26:29 +0200

    fix locate

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './homepageComponent/hero/hero.component';
 import { CarouselComponent } from './homepageComponent/carousel/carousel.component';
 import { LocateComponent } from './homepageComponent/locate/locate.component';
+import { BannerComponent } from './homepageComponent/banner/banner.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeroComponent, CarouselComponent, LocateComponent],
+  imports: [HeroComponent, CarouselComponent, LocateComponent, BannerComponent],
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
